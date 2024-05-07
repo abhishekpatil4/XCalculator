1f81f5057a2adca49dc99c9f782a973b50f64cf5
Abhishek Patil <abishkpatil@gmail.com>
2024-05-08 02:58:26 +0530

    feat: add calc UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,41 @@ import './App.css'
 
 function App() {
   const [cur, setCur] = useState("");
-
+  const [res, setRes] = useState();
   return <>
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
       <h1>React Calculator</h1>
-      <input type="text" value={cur} />
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} >
+      <input type="text" value={cur} style={{ margin: '30px', minWidth: "200px" }} />
+      {
+        res && 
+        <div style={{ marginBottom:'10px', fontSize:'25px', color:"gray"}}>
+          {res}
+        </div>
+      }
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }} >
         <div>
-          <button className="custom-button" onClick={() => setCur("hey it works!")}>7</button>
-          <button className="custom-button" onClick={() => setCur("hey it works!")}>8</button>
-          <button className="custom-button" onClick={() => setCur("hey it works!")}>9</button>
-          <button className="custom-button" onClick={() => setCur("hey it works!")}>+</button>
+          <button className="custom-button">7</button>
+          <button className="custom-button">8</button>
+          <button className="custom-button">9</button>
+          <button className="custom-button">+</button>
+        </div>
+        <div>
+          <button className="custom-button">4</button>
+          <button className="custom-button">5</button>
+          <button className="custom-button">6</button>
+          <button className="custom-button">-</button>
+        </div>
+        <div>
+          <button className="custom-button">1</button>
+          <button className="custom-button">2</button>
+          <button className="custom-button">3</button>
+          <button className="custom-button">*</button>
+        </div>
+        <div>
+          <button className="custom-button">C</button>
+          <button className="custom-button">0</button>
+          <button className="custom-button">=</button>
+          <button className="custom-button">/</button>
         </div>
       </div>
     </div>
