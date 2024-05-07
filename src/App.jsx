90608253436134ec3377801bed71a7de56c87a6a
Abhishek Patil <abishkpatil@gmail.com>
2024-05-08 02:50:51 +0530

    feat: input element & button styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
+import './App.css'
 
 function App() {
+  const [cur, setCur] = useState("");
+
   return <>
-    <h1>calculator</h1>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+      <h1>React Calculator</h1>
+      <input type="text" value={cur} />
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} >
+        <div>
+          <button className="custom-button" onClick={() => setCur("hey it works!")}>7</button>
+          <button className="custom-button" onClick={() => setCur("hey it works!")}>8</button>
+          <button className="custom-button" onClick={() => setCur("hey it works!")}>9</button>
+          <button className="custom-button" onClick={() => setCur("hey it works!")}>+</button>
+        </div>
+      </div>
+    </div>
   </>
 }
 export default App
